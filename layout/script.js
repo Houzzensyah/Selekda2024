6134d91e65cb9d5faaaf796971600ea4e4ef1cdf
ZennAdhyaksa <152697511+ZennAdhyaksa@users.noreply.github.com>
2024-08-29 14:56:51 +0700

    working layout

---
+++ layout/script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+    //initialisation global variable
+    const serviceButton = document.getElementById('service-button')
+    const portfolioButton = document.getElementById('portfolio-button')
+    const testimoniButton = document.getElementById('testimonial-button')
+    const newsButton = document.getElementById('news-button')
+    const landingPage = document.getElementById('landing-page')
+    const portfolio = document.getElementById('portfolio')
+    const blog = document.getElementById('blog');
+
+
+
+    portfolioButton.addEventListener('click', function () {
+        portfolio.style.display = 'block'
+        landingPage.style.display = 'none'
+    })
+
+    newsButton.addEventListener('click', function () {
+        landingPage.style.display = 'block'
+        portfolio.style.display = 'none'
+        blog.style.display = 'none'
+
+    })
+
+
+});
